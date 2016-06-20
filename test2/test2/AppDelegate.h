0679d4d4276f0920ceb0a8c5115cb4369ca7ac67
CF <colinfu@me.com>
2016-06-20 08:43:08 +0800

    init

---
+++ test2/test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by fuyou on 16/6/20.
+//  Copyright © 2016年 fuyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2/test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by fuyou on 16/6/20.
+//  Copyright © 2016年 fuyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
